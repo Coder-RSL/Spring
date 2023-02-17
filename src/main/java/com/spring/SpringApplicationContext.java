dff65572f154a3625f15a1fd745494442a224fd8
Coder-RSL <a13036923860@163.com>
2023-02-17 12:44:45 +0800

    将目录中的中文改为英文

--- src/main/java/com/spring/SpringApplicationContext.java
+++ src/main/java/com/spring/SpringApplicationContext.java
@@ -1,13 +1,39 @@
 package com.spring;
 
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+
 public class SpringApplicationContext {
     private Class confifClass;
 
     public SpringApplicationContext(Class confifClass) {
         this.confifClass = confifClass;
+
         //解析配置类
         //ComponentScan注解------>扫描路径----->扫描
+        //只探索Spring提供的注解
+        ComponentScan componentScanAnnotation = (ComponentScan) confifClass.getDeclaredAnnotation(ComponentScan.class);//拿到某一个注解
+        String path =componentScanAnnotation.value();//获得扫描路径
+        System.out.println(path);
 
+        // 扫描
+        // Bootstrap----->jre/lib
+        // Ext----------->jre/txt/lib
+        // APP----------->classpath
+
+        ClassLoader classLoader = SpringApplicationContext.class.getClassLoader();//app的classloader
+        URL resource = classLoader.getResource("com/zhouyu/service");//通过类加载器获取资源
+//相对路径（classpath） target里的文件
+
+        File file =new File(resource.getFile());
+        System.out.println(file);
+        if(file.isDirectory()){
+            File[] files = file.listFiles();
+            for(File f:files){
+                System.out.println(f);
+            }
+        }
     }
 
     public Object getBean(String beanName){
--- src/main/java/com/zhouyu/service/UserService.java
+++ src/main/java/com/zhouyu/service/UserService.java
@@ -5,4 +5,5 @@ import com.spring.Component;
 //@Component("userService")
 @Component("userService")//spring中会有一个规则
 public class UserService {
+
 }
